a93ff395f77f463209d63a0db766af744b5ffb88
mcu8rookie <394762391@qq.com>
2018-04-24 16:39:32 +0800

    add source

---
+++ source/user/main.c
@@ -0,0 +1,31 @@
+#ifndef __MAIN_C__
+#define __MAIN_C__
+
+#include "main.h"
+
+void main_init(void)
+{
+	return;
+}
+void main_mainloop(void)
+{
+	return;
+}
+
+int main(void)
+{
+	main_init();
+	
+	while(0!=1)
+	{
+		main_mainloop();
+	}
+	
+	// Occur some if run here.
+}
+
+#endif // #ifndef __MAIN_C__
+
+
+
+
